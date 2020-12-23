f5ce2c4575f668fdf9f78868236c811a652aeb79
Emmanuel Elegbede <emmanuelhemarxyll@gmail.com>
2020-12-23 22:13:01 +0000

    new

--- script.js
+++ script.js
@@ -1,54 +1,58 @@
-
-$(window).on("load", ()=>{
+//show welcoma modal on page load
+$(window).on("load", () => {
   $("#exampleModalCenter").modal("show");
 });
-const isValid = (element)=>{
-return element.name && element.value
+
+//form input to JSON
+const isValid = (element) => { //confirms the presence of input name and value
+  return element.name && element.value
 }
-const isChecked = element =>{
+const isChecked = element => {
+
   return !['checkbox', 'radio'].includes(element.type) || element.checked
 }
 const isCheckBox = element => element.type === "checkbox";
 
-const formToJSON_deconstructed = elements =>{
-  const reducerFunction=(data, element) =>{
-  data[element.name] = element.value;
-  console.log(JSON.stringify(data));
-  return data
-    }
-    const reducerInitialValue ={};
-    const formData = [].reduce.call(elements, reducerFunction, reducerInitialValue);
-return formData
-}
-const formToJSON = elements =>[].reduce.call(elements, (data, element) =>{
-  if(isValid(element) && isChecked(element)){  
-  if(isCheckBox(element)){
-  data[element.name] =(data[element.name] || []).concat(element.value)
+//returns form data as an object literal
+const formToJSON_deconstructed = elements => {
+  //funcction called on each element of the array
+  const reducerFunction = (data, element) => {
+    data[element.name] = element.value;
+    return data
   }
-  else data[element.name] = element.value
+  const reducerInitialValue = {};//initiaates the value of data as an empty object
+  const formData = [].reduce.call(elements, reducerFunction, reducerInitialValue);
+  return formData
+}
+const formToJSON = elements => [].reduce.call(elements, (data, element) => {
+  if (isValid(element) && isChecked(element)) {
+    if (isCheckBox(element)) {
+      data[element.name] = (data[element.name] || []).concat(element.value)
+    }
+    else data[element.name] = element.value
   }
   return data;
 }, {})
 
 const handleFormSubmit = e => {
   e.preventDefault()
- // e.stopPropagation()
+  e.stopPropagation()
 
-const data = formToJSON(form.elements);
-const dataDisplay = document.querySelectorAll(".data-display")[0];
-dataDisplay.textContent = JSON.stringify(data, null, " ");
+  const data = formToJSON(form.elements);
+  const dataDisplay = document.querySelectorAll(".data-display")[0];
+  dataDisplay.textContent = JSON.stringify(data, null, " ");
 }
 
 const form = document.getElementById("formdata");
 form.addEventListener('submit', handleFormSubmit);
 
+//Bootstrap form validator
 (function () {
   'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
+  var forms = document.querySelectorAll('.needs-validation')// Fetch all the forms we want to apply custom Bootstrap validation styles to
 
-var display = document.getElementById("code");
+  var display = document.getElementById("code"); //div within which form data would be shown
   var container = document.querySelector(".form");
   var code = document.querySelector("pre")
 
@@ -59,31 +63,30 @@ var display = document.getElementById("code");
         if (!form.checkValidity()) {
           event.preventDefault()
           event.stopPropagation()
-      display.style.display= "none"
-         
+          display.style.display = "none"
+
         }
-       else {
-        
-        code.classList.add("prettyprint")
-         display.style.display="block"
-         container.style.display= "none"
-         document.querySelector(".form-container").setAttribute("style", "display: flex; align-items: flex-start; justify-content: center; background-color:  hsl(225, 29%, 97%); height: 100vh; width: 100vw; border-radius: 0;")
-         document.body.setAttribute("style", "background-color: #fff; padding: 0")
-        //  document.querySelector("html").style.height= "100vh"
-       } 
-       validateCheckbox();
+        else {
+
+          code.classList.add("prettyprint")
+          display.style.display = "block"
+          container.style.display = "none"
+          document.querySelector(".form-container").setAttribute("style", "display: flex; align-items: flex-start; justify-content: center; background-color:  hsl(225, 29%, 97%); height: 100vh; width: 100vw; border-radius: 0;")
+          document.body.setAttribute("style", " background-color:  hsl(225, 29%, 97%); padding: 0")
+         }
+        validateCheckbox();
         form.classList.add('was-validated');
-                 
+
       }, false)
     })
 })();
 
 function validateCheckbox() {
-    if ($('input[type=checkbox]:checked').length > 0) {
-        $('input[type=checkbox]').prop('required', false);
-    } else {
-        $('input[type=checkbox]').prop('required', true);
-    }
+  if ($('input[type=checkbox]:checked').length > 0) {
+    $('input[type=checkbox]').prop('required', false);
+  } else {
+    $('input[type=checkbox]').prop('required', true);
+  }
 
 }
 
